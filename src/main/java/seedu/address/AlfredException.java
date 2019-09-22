097ca5972b57b54545e0a0631ef40f8c74440c48
Joanna-YJA <joannajoules@gmail.com>
2019-09-23 00:24:40 +0800

    Exception interface updates

---
+++ src/main/java/seedu/address/AlfredException.java
@@ -0,0 +1,22 @@
+package seedu.address;
+
+public abstract class AlfredException extends Exception {
+
+
+    public AlfredException(String message) {
+        super(message);
+    }
+
+    public AlfredException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
+
+    public AlfredException(Exception cause) {
+        super(cause);
+    }
+
+
+}
+
