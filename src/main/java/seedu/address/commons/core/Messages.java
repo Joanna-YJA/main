5d6568250397adcb061ecaab65763213a0f7e28f
Joanna-YJA <54109347+Joanna-YJA@users.noreply.github.com>
2019-09-28 16:54:22 +0800

    Update Messages.java

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -11,7 +11,7 @@ public class Messages {
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The entity index provided is invalid";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
-    //to set restrictions as some operations can only be applied to some entity
+    // to set restrictions as some operations can only be applied to some entity
     public static final String MESSAGE_INVALID_TYPE = "The type of entity is invalid";
 
 }
