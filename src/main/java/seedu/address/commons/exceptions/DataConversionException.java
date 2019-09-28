43df73c3a9c7bf7bc3d0925e326b3d875eb251b7
Joanna-YJA <54109347+Joanna-YJA@users.noreply.github.com>
2019-09-28 16:51:23 +0800

    Update DataConversionException.java

--- src/main/java/seedu/address/commons/exceptions/DataConversionException.java
+++ src/main/java/seedu/address/commons/exceptions/DataConversionException.java
@@ -6,6 +6,7 @@ import seedu.address.AlfredException;
  * Represents an error during conversion of data from one format to another
  */
 public class DataConversionException extends AlfredException {
+   
     /** Constructs an instance of DataConversionException.
      * @param cause Can have one of two main causes.
      * 1) IOException from error reading from JSON file
